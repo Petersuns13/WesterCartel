764cbfcc739efef96e2b64ba963ea476063fd7d3
YazOthman <32149256+YazOthman@users.noreply.github.com>
2017-09-26 20:45:22 +1000

    Comment Section

---
+++ index.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+
+<head>
+<title> Comment Section </title>
+<link href="/css/layout.css" rel="stylesheet"/>
+<script type= "text/javascript" src="/js/jquery.js"></script>
+<script type= "text/javascript" src="/js/jquery.js"></script>
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<div class="page-data">
+
+</div>
+
+<div class="comment-wrapper">
+
+</div>
+</div>
+</body>
+</html>
